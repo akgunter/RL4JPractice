ebf3c113c2d8b6acf759a7c19f05cb725ab978c6
Alexander Gunter <akgunter@go.olemiss.edu>
2018-07-18 14:29:09 -0500

    Corrected import line

--- src/main/scala/net/ddns/akgunter/rl4j_practice/RunAI.scala
+++ src/main/scala/net/ddns/akgunter/rl4j_practice/RunAI.scala
@@ -1,5 +1,7 @@
 package net.ddns.akgunter.rl4j_practice
 
+import net.ddns.akgunter.rl4j_practice.spark.CanSpark
+
 object RunAI extends CanSpark {
   def main(args: Array[String]): Unit = {
     println("Ran")
