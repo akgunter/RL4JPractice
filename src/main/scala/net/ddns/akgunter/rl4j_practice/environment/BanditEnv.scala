a042f132b14086c3399db13a340f87c80954067f
Alexander Gunter <akgunter@go.olemiss.edu>
2018-07-23 15:35:52 -0500

    Corrected reset() function

--- src/main/scala/net/ddns/akgunter/rl4j_practice/environment/BanditEnv.scala
+++ src/main/scala/net/ddns/akgunter/rl4j_practice/environment/BanditEnv.scala
@@ -7,11 +7,11 @@ import org.deeplearning4j.rl4j.mdp.MDP
 import org.deeplearning4j.rl4j.space.{DiscreteSpace, ObservationSpace, ArrayObservationSpace}
 import org.json.JSONObject
 
-class BanditEnv(numBandits: Int, numMachines: Int, maxNumTrials: Int) extends MDP[BanditTeamState, Integer, DiscreteSpace] {
+class BanditEnv(numBandits: Int, numMachines: Int, maxNumSteps: Int) extends MDP[BanditTeamState, Integer, DiscreteSpace] {
   protected var observationSpace: ArrayObservationSpace[BanditTeamState] = new ArrayObservationSpace(Array(numMachines))
   protected val actionSpace: DiscreteSpace = new DiscreteSpace(numMachines)
   protected var currentState: BanditTeamState = new BanditTeamState(numBandits)
-  protected var numTrialsTaken = 0
+  protected var numStepsTaken = 0
   protected val distributions: Array[Array[Double]] = {
     Array.fill(numBandits)(Array.fill(numMachines)(Random.nextDouble))
   }
@@ -23,6 +23,7 @@ class BanditEnv(numBandits: Int, numMachines: Int, maxNumTrials: Int) extends MD
   override def reset(): BanditTeamState = {
     observationSpace = new ArrayObservationSpace(Array(numMachines))
     currentState = new BanditTeamState(numBandits)
+    numStepsTaken = 0
     currentState
   }
 
@@ -32,14 +33,16 @@ class BanditEnv(numBandits: Int, numMachines: Int, maxNumTrials: Int) extends MD
     val threshold = distributions(currentState.getCurBandit)(action)
     val reward = if (Random.nextDouble > threshold) 1 else -1
     currentState = currentState.getNextState
-    numTrialsTaken += 1
+    numStepsTaken += 1
 
     new StepReply(currentState, reward, isDone, new JSONObject("{}"))
   }
 
-  override def isDone: Boolean = numTrialsTaken == maxNumTrials
+  override def isDone: Boolean = {
+    numStepsTaken == maxNumSteps
+  }
 
   override def newInstance(): MDP[BanditTeamState, Integer, DiscreteSpace] = {
-    new BanditEnv(numBandits, numMachines, maxNumTrials)
+    new BanditEnv(numBandits, numMachines, maxNumSteps)
   }
 }
